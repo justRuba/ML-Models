d2087741dd7b09a85e0f9cb69462fa7c798d97cd
justRuba <ruba8800@gmail.com>
2026-01-01 14:19:42 +0300

    Initial commit: add ML models scripts

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import numpy as np
 
 def classification_metrics(y_true, y_pred):
     return {
@@ -9,10 +10,16 @@ def classification_metrics(y_true, y_pred):
         "F1 Score": f1_score(y_true, y_pred, average='weighted')
     }
 
+
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import numpy as np
+
 def regression_metrics(y_true, y_pred):
+    mse = mean_squared_error(y_true, y_pred)
+    rmse = np.sqrt(mse)  # manual RMSE
+
     return {
-        "MSE": mean_squared_error(y_true, y_pred),
-        "RMSE": mean_squared_error(y_true, y_pred, squared=False),
-        "MAE": mean_absolute_error(y_true, y_pred),
-        "R2": r2_score(y_true, y_pred)
+        "MSE": mse,
+        "RMSE": rmse
     }
